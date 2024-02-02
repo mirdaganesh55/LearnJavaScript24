b73c14e27450877dc70d79c592a2a212a20fd27a
mirdaganesh55 <ganeshmirda5@gmail.com>
2024-02-02 18:11:59 +0530

    AttributeDOM2

---
+++ LearnJS/src/main/webapp/JsDOM2.js
@@ -0,0 +1,12 @@
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+let p = document.querySelector("p");
+//console.log(p.getAttribute("class"));
+console.log(p.setAttribute("class","New-Para"));
